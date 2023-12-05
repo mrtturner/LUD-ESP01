f184c014cb9d880b90c1f8d6d744c4152d076c8f
mrtturner <46408264+mrtturner@users.noreply.github.com>
2023-12-05 22:56:22 +0000

    Update boot.py

--- app/boot.py
+++ app/boot.py
@@ -27,46 +27,56 @@ def codeOTA():
     import senko
     OTA = senko.Senko(user=GITUSER, repo=GITREPO, working_dir=GITDIR, files=GITFILES)
         
-    if OTA.fetch():
-        print("A newer version is available!")
-        # RGB Flash
-        for i in range(NUMLEDS):
-            np[i] = (0, 0, 0)  # GRB format
+    try:
+        if OTA.fetch():
+            print("A newer version is available!")
+            # RGB Flash
+            for i in range(NUMLEDS):
+                np[i] = (0, 0, 0)  # GRB format
 
-        np[0] = (0, 255, 0)  # GRB format
-        np[1] = (255, 0, 0)  # GRB format
-        np[2] = (0, 0, 255)  # GRB format
-        np.write()
-        start = time.ticks_ms()
-        while time.ticks_diff(time.ticks_ms(), start) < 1000:  # Loop for 1 second
-            machine.idle()
-        np[2] = (0, 255, 0)  # GRB format
-        np[0] = (255, 0, 0)  # GRB format
-        np[1] = (0, 0, 255)  # GRB format
-        np.write()
-        start = time.ticks_ms()
-        while time.ticks_diff(time.ticks_ms(), start) < 1000:  # Loop for 1 second
-            machine.idle()
-        np[1] = (0, 255, 0)  # GRB format
-        np[2] = (255, 0, 0)  # GRB format
-        np[0] = (0, 0, 255)  # GRB format
-        np.write()
-        start = time.ticks_ms()
-        while time.ticks_diff(time.ticks_ms(), start) < 1000:  # Loop for 1 second
-            machine.idle()
-        np[0] = (0, 255, 0)  # GRB format
-        np[1] = (255, 0, 0)  # GRB format
-        np[2] = (0, 0, 255)  # GRB format
-        np.write()
-        start = time.ticks_ms()
-        while time.ticks_diff(time.ticks_ms(), start) < 1000:  # Loop for 1 second
-            machine.idle()
-            
-        if OTA.update():
-            print("Updated to the latest version! Rebooting...")
-            machine.reset()
-    else:
-        print("Up to date!")
+            np[0] = (0, 255, 0)  # GRB format
+            np[1] = (255, 0, 0)  # GRB format
+            np[2] = (0, 0, 255)  # GRB format
+            np.write()
+            start = time.ticks_ms()
+            while time.ticks_diff(time.ticks_ms(), start) < 1000:  # Loop for 1 second
+                machine.idle()
+            np[2] = (0, 255, 0)  # GRB format
+            np[0] = (255, 0, 0)  # GRB format
+            np[1] = (0, 0, 255)  # GRB format
+            np.write()
+            start = time.ticks_ms()
+            while time.ticks_diff(time.ticks_ms(), start) < 1000:  # Loop for 1 second
+                machine.idle()
+            np[1] = (0, 255, 0)  # GRB format
+            np[2] = (255, 0, 0)  # GRB format
+            np[0] = (0, 0, 255)  # GRB format
+            np.write()
+            start = time.ticks_ms()
+            while time.ticks_diff(time.ticks_ms(), start) < 1000:  # Loop for 1 second
+                machine.idle()
+            np[0] = (0, 255, 0)  # GRB format
+            np[1] = (255, 0, 0)  # GRB format
+            np[2] = (0, 0, 255)  # GRB format
+            np.write()
+            start = time.ticks_ms()
+            while time.ticks_diff(time.ticks_ms(), start) < 1000:  # Loop for 1 second
+                machine.idle()
+                
+            if OTA.update():
+                print("Updated to the latest version! Rebooting...")
+                machine.reset()
+        else:
+            print("Up to date!")
+            wlan = network.WLAN(network.STA_IF)
+            wlan.active(False)
+            print("Wifi disabled. Restart device to check for updates again.")
+            print("Running main program")
+            gc.collect()
+            import main
+            main.mainloop()
+    except Exception as e:
+        print("Failed to perform OTA update:", str(e))
         wlan = network.WLAN(network.STA_IF)
         wlan.active(False)
         print("Wifi disabled. Restart device to check for updates again.")
@@ -108,6 +118,7 @@ def main():
     if scan_for_ssid(SSID_TO_CONNECT, wlan):
         if connect_to_wifi(SSID_TO_CONNECT, WIFI_PASSWORD):
             print("Connected to", SSID_TO_CONNECT)
+            gc.collect()
             codeOTA()
         else:
             print("Failed to connect, turning off Wi-Fi")
@@ -118,3 +129,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
